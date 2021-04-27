a0168ef3b8b6d61d503b6d105936797339588806
kjones <jones.kjg@gmail.com>
2021-04-27 13:08:29 -0400

    create helper functions for updating score and shuffling cards

---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+import AsyncStorage from '@react-native-community/async-storage'
+
+const SCORE_KEY = 'MobileFlashcards:score'
+
+export function updateScore(numCorrect) {
+    return AsyncStorage.mergeItem(SCORE_KEY, numCorrect)
+}
+
+export function shuffleDeck(deck) {
+    for (let i = deck.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1))
+        [deck[i], deck[j]] = [deck[j], deck[i]]
+    }
+    return deck
+}
