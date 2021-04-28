bd6e68ed5aba3ce547d9b61df0601c6a37b7df08
kjones <jones.kjg@gmail.com>
2021-04-28 10:44:01 -0400

    move FlashCard component code to CardContainer component

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native'
 import FlashCard from './FlashCard'
 import { updateScore } from '../utils/helpers'
+import { Feather } from '@expo/vector-icons'
 
+function FlipIcon ({ onPress }) {
+    return (
+        <View>
+            <TouchableOpacity onPress={onPress}>
+                <Feather />
+            </TouchableOpacity>
+        </View>
+    )
+}
 class CardContainer extends Component {
     state = {
-        enabled: false,
+        flipped: false,
         numCorrect: 0,
         curIndex: 0
     }
@@ -18,44 +28,58 @@ class CardContainer extends Component {
 
     updateIndex = (isLast) => {
         if (isLast === true){
-            updateScore(this.state.numCorrect)
-            .then(() => {this.props.navigation.navigate('Score', {numCorrect: this.state.numCorrect, title: this.props.title})})
+            const { numCorrect } = this.state
+            updateScore(numCorrect)
+            .then(() => {this.props.navigation.navigate('Score', {numCorrect: numCorrect, title: this.props.title})})
         }
         else{
             this.setState((prevState) => ({
                 curIndex: prevState.curIndex + 1,
-                enabled: false
+                flipped: false
             }))
         }
     }
 
-    updateCorrect = () => {
+    updateCorrect = (isLast) => {
         this.setState((prevState) => ({
             numCorrect: prevState.numCorrect + 1
         }))
+        this.updateIndex(isLast)
+
+        
     }
+
+    flipCard = () => {
+        this.setState(() => ({
+            flipped: true
+        }))
+    }
+
 
     render(){
         const { deck } = this.props
+        const questions = deck.questions
         const last = this.state.curIndex === deck.questions.length - 1
+        const { flipped, curIndex } = this.state
         return (
             <View>
-                {deck.map((elem) => (
-                    <FlashCard 
-                        key={elem.question} 
-                        question={elem.question} 
-                        answer={elem.answer}
-                        handleFlip={this.enableButtons}
-                        />
-                ))}
-
+                {flipped === false 
+                    ? <View>
+                        <Text>{questions[curIndex].question}</Text>  
+                        <FlipIcon onPress={this.flipCard}/>
+                    </View>
+                    : 
+                    <View>{questions[curIndex].answer}</View>}
+                                
+                <TouchableOpacity disabled={flipped===false} onPress={this.updateCorrect(last)}>
+                    Correct
+                </TouchableOpacity>
+                <TouchableOpacity disabled={flipped===false} onPress={this.updateIndex(last)}>
+                    Incorrect
+                </TouchableOpacity>
             </View>
         )
     }
 }
 
-function mapStateToProps( decks ) {
-    return {
-        decks
-    }
-}
+export default CardContainer
