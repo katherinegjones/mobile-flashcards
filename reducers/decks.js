cfb317ad3814cafa5bd6cb1e121e840d80570ce8
kjones <jones.kjg@gmail.com>
2021-04-30 20:41:48 -0400

    consolidate reducers

---
+++ reducers/decks.js
@@ -0,0 +1,37 @@
+import { RECEIVE_DECKS, ADD_DECK, RECEIVE_DECK, ADD_CARD } from '../actions/decks'
+
+export default function decks (state = {}, action) {
+    switch (action.type) {
+        case RECEIVE_DECKS:
+            return {
+                ...state,
+                ...action.entries
+            }
+        case ADD_DECK: 
+            return {
+                ...state,
+                ...action.info
+            }
+        case RECEIVE_DECK:
+            return {
+                ...state,
+                ...action.cards
+            }
+        case ADD_CARD:
+            const obj = {
+                question: action.question,
+                answer: action.answer
+            }
+            return {
+               ...state,
+               [action.title] : {
+                   questions: state[action.title].questions.concat(obj)
+               }
+            }
+            
+        default:
+            return state
+    }
+
+
+}
