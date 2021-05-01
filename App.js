e4c2bc5df221889fb2abe4a412cde2ae8b651f44
kjones <jones.kjg@gmail.com>
2021-04-30 20:43:11 -0400

    create stack navigator component

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import DeckList from './components/DeckList';
+import CardContainer from './components/CardContainer';
+import Deck from './components/Deck'
+import Score from './components/Score'
+import AddCard from './components/AddCard'
+import AddDeck from './components/AddDeck'
+import { addCard } from './actions/cards';
+
+
+const Stack = createStackNavigator()
+
+const StackNavigatorConfigs = {
+  headerMode: 'screen'
+}
+
+const MainNav = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='DeckList' component={DeckList}/>
+    <Stack.Screen name='AddDeck' component={AddDeck} />
+    <Stack.Screen name='CardContainer' component={CardContainer}/>    
+    <Stack.Screen name='Deck' component={Deck}/>
+    <Stack.Screen name='Score' component={Score} />    
+    <Stack.Screen name='AddCard' component={AddCard} />
+  </Stack.Navigator>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <MainNav />
+        </NavigationContainer>
+      </View>
+    </Provider>
   );
 }
 
