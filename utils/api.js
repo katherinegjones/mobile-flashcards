3952c82e188df77035db356c8a6e429851f2ae9c
kjones <jones.kjg@gmail.com>
2021-05-02 12:44:29 -0400

    fix functions for adding cards and decks

--- utils/api.js
+++ utils/api.js
@@ -1,29 +1,50 @@
-import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { shuffleDeck } from './helpers'
 
 const DECK_STORAGE_KEY = 'MobileFlashcards:deck'
 
 
-
-
 export function getDecks() {
+    //AsyncStorage.clear()
     return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    //.then((results) => console.log("Received results: ", JSON.parse(results)))
 }
 
-export function getDeck(title){
+export function getShuffledDeck(title){
     return AsyncStorage.getItem(DECK_STORAGE_KEY)
     .then((results) => {
         const data = JSON.parse(results)
-        return obj[title]
+        const deck = data[title].questions
+        const shuffled = shuffleDeck(deck)
+        const newData = {
+            ...data,
+            [title]: {
+                questions: shuffled
+            }
+        }
+
+        return data[title].questions
     })
 }
 
-export function submitDeck(title) {
-    return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
-        [title]: {}
-    }))
-}
+export function saveDeckTitle(title) {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((results) => {
+        const data = JSON.parse(results)
+        //console.log(data)
+            AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
+                [title]: {
+                    questions: []
+                    }
+                }))
+        }
+    )  
+    }
 
-export function submitCard(title, question, answer){
+    
+
+
+export function addCardToDeck( title, question, answer ){
     return AsyncStorage.getItem(DECK_STORAGE_KEY)
     .then((results) => {
         const data = JSON.parse(results)
@@ -31,10 +52,14 @@ export function submitCard(title, question, answer){
             question: question,
             answer: answer
         }
-        data[title] = {
-            ...data[title],
-            questions: data[title].questions.concat(obj)
+        const newData = {
+            ...data,
+            [title] : {
+                questions: data[title].questions ? data[title].questions.concat(obj) : obj
+            }
         }
-        AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data))
+        
+        console.log(newData)
+        AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(newData))
     })
 }
