1585fa420898160ef5391f1862a5d7fabb629342
kjones <jones.kjg@gmail.com>
2021-05-14 21:00:18 -0400

    fix shuffle functionality, add home button

--- components/Score.js
+++ components/Score.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, Platform, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
-import universal from './Styles'
+import shared from './Styles'
 import { getShuffledDeck } from '../utils/api'
 import { receiveDecks } from '../actions/decks'
 import { FontAwesome5, Entypo } from '@expo/vector-icons'
 
 class Score extends Component {
     reShuffle = async () => {
-        const { deckTitle, dispatch } = this.props
-        getShuffledDeck(deckTitle) 
+        const { deckTitle, receiveDecks, navigation } = this.props
+        await getShuffledDeck(deckTitle) 
         .then((results) => {
-            dispatch(receiveDecks(results))
+            receiveDecks(results)
         })
+        .then(() => navigation.navigate('QuizView', {title: deckTitle}))
     }
     render() {
         const { deckTitle, numCorrect, total, navigation } = this.props
         return (
-            <View style={universal.container}>
+            <View style={shared.container}>
                 <Text>Your Score:</Text>
                 <Text>{numCorrect}/{total}</Text>
                 <TouchableOpacity 
-                    onPress={() => {navigation.navigate('DeckList')}}
-                    style={universal.button}
+                    onPress={() => {navigation.navigate('Deck', {title: {deckTitle}})}}
+                    style={shared.button}
                     >
                         <Entypo name='arrow-bold-left' color='linen' size={15}/>
-                    <Text style={[universal.buttonText, {marginLeft: 5}]}>Back to Main</Text>
+                    <Text style={[shared.buttonText, {marginLeft: 5}]}>Back to Deck</Text>
                 </TouchableOpacity>
                 <TouchableOpacity 
-                    onPress={async () => {this.reShuffle() .then(() => navigation.navigate('QuizView', {title: deckTitle}))}}
-                    style={universal.button}
+                    onPress={this.reShuffle}
+                    style={shared.button}
                     >
-                    <Text style={[universal.buttonText, {marginRight: 5}]}>Replay</Text>
+                    <Text style={[shared.buttonText, {marginRight: 5}]}>Replay</Text>
                     <FontAwesome5 name='redo-alt' color='linen'/>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('DeckList')}
+                    style={shared.button}
+                >
+                    <Entypo name='home' color='linen'/>
+                    <Text style={[shared.buttonText, {marginLeft: 5}]}>Back to Main</Text>
                 </TouchableOpacity>
             </View>
         )
@@ -42,14 +50,18 @@ class Score extends Component {
 
 function mapStateToProps(state, { route }) {
     const { numCorrect, deckTitle } = route.params
-    const deck = state[deckTitle]
-    const total = deck.questions.length
+    const total = state[deckTitle].questions.length
     return {
-        deck,
         deckTitle, 
         numCorrect,
         total
     }
 }
 
-export default connect(mapStateToProps)(Score)
+function mapDispatchToProps(dispatch) {
+    return {
+        receiveDecks: (decks) => dispatch(receiveDecks(decks))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Score)
